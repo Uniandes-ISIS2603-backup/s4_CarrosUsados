e36c487523a1f495946251389359ff9d65c0c2e8
estudiante <estudiante@ISIS2603-381.sis.virtual.uniandes.edu.co>
2018-08-30 00:32:43 -0500

    Se añadio el codigo de los recursos Automovil y FichaTecnica, con sus respectivas clases DTO.

---
+++ s4_CarrosUsados-api/src/main/java/co/edu/uniandes/csw/carrosUsados/dtos/AutomovilDTO.java
@@ -0,0 +1,36 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package co.edu.uniandes.csw.carrosUsados.dtos;
+
+import java.io.Serializable;
+import java.util.Date;
+
+/**
+ *
+ * @author estudiante
+ */
+
+public class AutomovilDTO implements Serializable{
+    
+    private long id;
+    private int modelo;
+    private String marca;
+    private int anio;
+    private String color;
+    private String num_chasis;
+    private String placa;
+    private Date fecha_agregacion; //Chequear el tipo de dato
+    private String precio_original;
+    
+    //Relacion a FichaTecnicaDTO dado que esta tiene cardinalidad 1
+    private FichaTecnicaDTO ficha_tecnica;
+    
+    public AutomovilDTO(){
+        
+    }
+    
+    
+}
