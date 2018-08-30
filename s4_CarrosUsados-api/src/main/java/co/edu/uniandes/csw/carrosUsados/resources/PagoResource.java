cfbeab580415515f725071b89a0d8eff05c9fb40
estudiante <estudiante@ISIS2603-343.sis.virtual.uniandes.edu.co>
2018-08-29 19:20:14 -0500

    Primero

---
+++ s4_CarrosUsados-api/src/main/java/co/edu/uniandes/csw/carrosUsados/resources/PagoResource.java
@@ -0,0 +1,50 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package co.edu.uniandes.csw.carrosUsados.resources;
+import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.WebApplicationException;
+import co.edu.uniandes.csw.carrosUsados.dtos.PagoDTO;
+import co.edu.uniandes.csw.carrosUsados.exceptions.BusinessLogicException;
+import java.util.ArrayList;
+import java.util.List;
+import javax.enterprise.context.RequestScoped;
+import javax.ws.rs.Consumes;
+import java.util.logging.Logger;
+/**
+ *
+ * @author estudiante
+ */
+@Path("pagos")
+@Produces("application/json")
+@Consumes("application/json")
+@RequestScoped
+public class PagoResource {
+    private final static Logger LOGGER = Logger.getLogger(PagoResource.class.getName());
+    public PagoResource()
+    {
+        
+    }
+    @GET
+    public List<PagoDTO> getPagos()  throws BusinessLogicException
+    {
+        
+       return new ArrayList<>();
+    }
+   
+    @POST
+    public PagoDTO createPago(PagoDTO pago) throws BusinessLogicException
+    {
+        return pago;
+    }
+   
+    
+}
